2159427cf6da49e9d970cb15bb09422e9da440ec
Peter Dowdy <peter.dowdy@gmail.com>
2021-12-16 21:27:36 -0800

    Day 17

---
+++ day17-2.py
@@ -0,0 +1,43 @@
+from itertools import *
+import heapq
+from collections import *
+
+
+# target_area = ((287, -76), (309, -48))
+min_x = 287
+max_x = 309
+min_y = -76
+max_y = -48
+
+# min_x = 20
+# max_x = 30
+# min_y = -5
+# max_y = 10
+
+positions = {}
+
+tests = []
+for x in range(1, 1000):
+    for y in range(min_y, 1000):
+        tests.append((x, y))
+
+for test in tests:
+    current_pos = (0, 0)
+    current_vec = test
+    y_max = 0
+    while True:
+        current_pos = (current_pos[0] + current_vec[0], current_pos[1] + current_vec[1])
+        y_max = max(y_max, current_pos[1])
+        current_vec = (max(current_vec[0] - 1, 0), current_vec[1] - 1)
+        if current_pos[0] > max_x or current_pos[1] < min_y:
+            break
+        elif (
+            current_pos[0] >= min_x
+            and current_pos[0] <= max_x
+            and current_pos[1] >= min_y
+            and current_pos[1] <= max_y
+        ):
+            positions[test] = y_max
+            break
+
+print(len(positions))
